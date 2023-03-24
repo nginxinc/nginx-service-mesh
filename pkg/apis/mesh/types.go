628edea8bee3ddbb4a8574859ad8179d4fb3fbbd
Joe <j.mcguire@f5.com>
2023-03-23 20:20:44 -0600

    rename ping channel to preserve upgrade capability

--- pkg/apis/mesh/types.go
+++ pkg/apis/mesh/types.go
@@ -55,8 +55,8 @@ const (
 	NatsAgentConfigChannel = "nginx.nsm.agent.config"
 	// NatsAgentSubChannel sends a subscription and version notice from agent to mesh-controller.
 	NatsAgentSubChannel = "nginx.nsm.agent.subscription"
-	// NatsControllerPingChannel sends a ping from mesh-controller to agent on restart.
-	NatsControllerPingChannel = "nginx.nsm.controller.ping"
+	// NatsAPIPingChannel sends a ping from mesh-controller to agent on restart.
+	NatsAPIPingChannel = "nginx.nsm.controller.ping"
 )
 
 // k8s static resource names.
