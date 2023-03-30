4685eee8fbda5eb11fcf95fe20de379374ba719d
Jared Byers <j.byers@f5.com>
2023-03-30 09:33:03 -0700

    Convert optional types to pointers; rename file

--- pkg/apis/mesh/v1alpha1/retrytimeoutconfig_types.go
+++ pkg/apis/mesh/v1alpha1/retrytimeoutconfig_types.go
@@ -14,22 +14,19 @@ type RetryTimeoutConfig struct {
 	// Spec defines the retry and timeout spec for requests to a target.
 	Spec RetryTimeoutConfigSpec `json:"spec"`
 
-	metav1.TypeMeta `json:",inline"`
-	// Standard object's metadata.
-	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
-	// +optional
+	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 }
 
 // RetryTimeoutConfigSpec defines the retry and timeout configuration for a target.
 type RetryTimeoutConfigSpec struct {
-	// TargetRef defines which target the retry and timeout applies to.
-	TargetRef TargetRefSpec `json:"targetRef"`
-
 	// Default is the default policy configuration to apply to the targetRef.
 	// Currently this isn't useful, but adding it now to minimize changes
 	// once override values come into play. That's also why "omitempty" is there.
 	Default RetryTimeoutDefaultSpec `json:"default,omitempty"`
+
+	// TargetRef defines which target the retry and timeout applies to.
+	TargetRef TargetRefSpec `json:"targetRef"`
 }
 
 // TargetRef defines the resource a policy is applied to.
@@ -50,11 +47,11 @@ type RetryTimeoutDefaultSpec struct {
 	// Valid timeouts are integers followed immediately by "m", "s", or "ms" to
 	// specify if the timeouts are minutes, seconds or milliseconds. Example: "500ms"
 	// +optional
-	Timeout string `json:"timeout,omitempty"`
+	Timeout *string `json:"timeout,omitempty"`
 
 	// Retry is the configuration for retrying failed requests to the targetRef.
 	// +optional
-	Retry RetrySpec `json:"retry,omitempty"`
+	Retry *RetrySpec `json:"retry,omitempty"`
 }
 
 // RetrySpec is the configuration for retrying failed requests to the targetRef.
@@ -66,7 +63,7 @@ type RetrySpec struct {
 
 	// Backoff is the configuration for exponentionally backing off between retries.
 	// +optional
-	Backoff BackoffSpec `json:"backoff,omitempty"`
+	Backoff *BackoffSpec `json:"backoff,omitempty"`
 }
 
 // BackoffSpec is the configuration for exponentionally backing off between retries.
@@ -75,13 +72,13 @@ type BackoffSpec struct {
 	// Subsequent retries will be delayed exponentially based on the initial interval.
 	// Example: with InitialInterval=1s, first retry is 1s, second, is 2s, third is 4s, etc.
 	// +optional
-	InitialInterval string `json:"initialInterval,omitempty"`
+	InitialInterval *string `json:"initialInterval,omitempty"`
 
 	// MaxInterval is the maximum delay between retries.
 	// Once the max interval is reached by exponentionally backing off
 	// all subsequent retries delay for the max interval.
 	// +optional
-	MaxInterval string `json:"maxInterval,omitempty"`
+	MaxInterval *string `json:"maxInterval,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
